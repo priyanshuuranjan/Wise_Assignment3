8531f00a059b777c381e9b139be711e63703a6a8
Priyanshu Rajan <priyanshumth0808@gmail.com>
2023-02-15 20:22:23 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    if (count < 10) {
+      setCount(count + 1);
+    }
+  };
+
+  const decrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  let color = 'green';
+  if (count >= 5 && count <= 9) {
+    color = 'blue';
+  } else if (count === 10) {
+    color = 'red';
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <h1 style={{ fontSize: '48px', fontWeight: 'bold', color: color, margin: '0 20px' }}>{count}</h1>
+      <button style={{ backgroundColor: '#fff', border: '2px solid #ccc', fontSize: '24px', padding: '10px', cursor: 'pointer' }} onClick={increment}>+</button>
+      <button style={{ backgroundColor: '#fff', border: '2px solid #ccc', fontSize: '24px', padding: '10px', cursor: 'pointer' }} onClick={decrement}>-</button>
     </div>
   );
 }
 
-export default App;
+export default Counter;
